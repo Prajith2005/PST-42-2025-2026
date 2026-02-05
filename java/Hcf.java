afa43274f98a7c346553cf06aa4cbd4147357366
PRAJITH R <prajithpredator1@gmail.com>
2026-02-05 10:25:00 +0530

    Add files via upload

---
+++ java/Hcf.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Hcf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int result = hcf(num1, num2);
+        System.out.println("HCF of " + num1 + " and " + num2 + " is: " + result);
+        sc.close();
+    }
+
+    static int hcf(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return hcf(b, a % b);
+    }
+}
